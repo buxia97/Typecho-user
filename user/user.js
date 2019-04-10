ddb0b1f26596d3c08566e7a8e994486d889f9a67
buxia97 <39846979+buxia97@users.noreply.github.com>
2019-04-10 11:55:24 +0800

    增加了后台的导航，配合实现后台管理界面

--- user/user.js
+++ user/user.js
@@ -1,4 +1,4 @@
-// typecho user V1.02
+// typecho user V1.10
 // 不暇 QQ1378283361
 // www.ruletree.club
 function getElementsClass(classnames){ 
@@ -16,14 +16,17 @@ function getElementsClass(classnames){
 	return classobj;
 } 
 window.onload=function (){
-  	if(UserGroup != "subscriber"){
-		var HtmlText="<div class=\"user-info\"><a href=\""+UserLink+"\"><img src=\""+UserPic+"s=100\" /></a><p>欢迎您，<a href=\""+UserLink+"\">"+UserName+"</a></p></div><div class=\"user-nav\"><ul><li><a href=\""+SiteLink+"admin/index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+SiteLink+"admin/profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li><li><a href=\""+SiteLink+"admin/write-post.php\"><i class=\"fa fa-pencil\"></i>创建文章</a></li><li><a href=\""+SiteLink+"admin/manage-posts.php\"><i class=\"fa fa-cube\"></i>管理文章</a></li><li><a href=\""+SiteLink+"admin/manage-comments.php\"><i class=\"fa fa-comments-o\"></i>管理评论</a></li></ul></div><div class=\"copy-links\"><p>版权链接</p><div class=\"links-mian\"><a href=\"\">typecho</a><a href=\"\">规则之树</a><a href=\"\">QQ群</a></div></div>";
-    }else{
-	var HtmlText="<div class=\"user-info\"><a href=\""+UserLink+"\"><img src=\""+UserPic+"s=100\" /></a><p>欢迎您，<a href=\""+UserLink+"\">"+UserName+"</a></p></div><div class=\"user-nav\"><ul><li><a href=\""+SiteLink+"admin/index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+SiteLink+"admin/profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li></ul></div><div class=\"copy-links\"><p>版权链接</p><div class=\"links-mian\"><a href=\"\">typecho</a><a href=\"\">规则之树</a><a href=\"\">QQ群</a></div></div>";
-    }
+	var UserInfo = "<div class=\"user-info\"><a href=\""+UserLink+"\"><img src=\""+UserPic+"s=100\" /></a><p>欢迎您，<a href=\""+UserLink+"\">"+UserName+"</a></p></div>";
+  	if(UserGroup == "administrator"){
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+SiteLink+"admin/index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-paper-plane\"></i>全局模块</a><ul><li><a href=\""+SiteLink+"admin/profile.php\">个人设置</a></li><li><a href=\""+SiteLink+"admin/plugins.php\">插件控制</a></li><li><a href=\""+SiteLink+"admin/themes.php\">模板外观</a></li><li><a href=\""+SiteLink+"admin/backup.php\">数据备份</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-pencil\"></i>快捷操作</a><ul><li><a href=\""+SiteLink+"admin/write-post.php\">撰写文章</a></li><li><a href=\""+SiteLink+"admin/write-page.php\">创建页面</a></li><li><a href=\""+SiteLink+"admin/user.php\">新增用户</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-cube\"></i>内容管理</a><ul><li><a href=\""+SiteLink+"admin/manage-posts.php\">内容文章</a></li><li><a href=\""+SiteLink+"admin/manage-pages.php\">独立页面</a></li><li><a href=\""+SiteLink+"admin/manage-comments.php\">用户评论</a></li><li><a href=\""+SiteLink+"admin/manage-categories.php\">分类管理</a></li><li><a href=\""+SiteLink+"admin/manage-tags.php\">标签管理</a></li><li><a href=\""+SiteLink+"admin/manage-medias.php\">文件管理</a></li><li><a href=\""+SiteLink+"admin/manage-users.php\">用户管理</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-gear\"></i>网站设置</a><ul><li><a href=\""+SiteLink+"admin/options-general.php\">基本设置</a></li><li><a href=\""+SiteLink+"admin/options-discussion.php\">评论设置</a></li><li><a href=\""+SiteLink+"admin/options-reading.php\">阅读设置</a></li><li><a href=\""+SiteLink+"admin/options-permalink.php\">永久链接</a></li></ul><li></ul></div>";
+	}else if(UserGroup == "editor"||UserGroup == "contributor"){
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+SiteLink+"admin/index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+SiteLink+"admin/profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li><li><a href=\""+SiteLink+"admin/write-post.php\"><i class=\"fa fa-pencil\"></i>创建文章</a></li><li><a href=\""+SiteLink+"admin/manage-posts.php\"><i class=\"fa fa-cube\"></i>管理文章</a></li><li><a href=\""+SiteLink+"admin/manage-comments.php\"><i class=\"fa fa-comments-o\"></i>管理评论</a></li></ul></div>";
+	}else{
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+SiteLink+"admin/index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+SiteLink+"admin/profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li></ul></div>";
+	}
 	var Nav = document.getElementById('typecho-nav-list');
 	if(UserGroup != ""){
-		Nav.innerHTML = HtmlText;
+		Nav.innerHTML = UserInfo+HtmlText;
       	var ToMain=getElementsClass("operate")[0];
         var Main=getElementsClass("main")[0];
         var ToNav=document.createElement('a');
@@ -77,4 +80,4 @@ window.onload=function (){
         Body.appendChild(Cover);
         LoginMain.innerHTML="<img src=\""+SiteLink+"user/logo.png\" alt=\""+SiteName+"\"/>";
     }
-}
+};
