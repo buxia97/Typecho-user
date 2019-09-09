697e5b429467da76cd3a532323014cd87c2dc438
buxia97 <39846979+buxia97@users.noreply.github.com>
2019-09-09 18:27:15 +0800

    界面效果日常优化

--- user/user.js
+++ user/user.js
@@ -1,4 +1,4 @@
-// typecho user V1.10
+// typecho user V1.11
 // 不暇 QQ1378283361
 // www.ruletree.club
 function getElementsClass(classnames){ 
@@ -18,7 +18,7 @@ function getElementsClass(classnames){
 $(function(){
 	var UserInfo = "<div class=\"user-info\"><a href=\""+AdminLink+"\"><img src=\""+UserPic+"s=100\" /></a><p>欢迎您，<a href=\""+AdminLink+"\">"+UserName+"</a></p></div>";
   	if(UserGroup == "administrator"){
-		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+AdminLink+"index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-paper-plane\"></i>全局模块</a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"profile.php\">个人设置</a></li><li><a href=\""+AdminLink+"plugins.php\">插件控制</a></li><li><a href=\""+AdminLink+"themes.php\">模板外观</a></li><li><a href=\""+AdminLink+"backup.php\">数据备份</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-pencil\"></i>快捷操作</a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"write-post.php\">撰写文章</a></li><li><a href=\""+AdminLink+"write-page.php\">创建页面</a></li><li><a href=\""+AdminLink+"user.php\">新增用户</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-cube\"></i>内容管理</a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"manage-posts.php\">内容文章</a></li><li><a href=\""+AdminLink+"manage-pages.php\">独立页面</a></li><li><a href=\""+AdminLink+"manage-comments.php\">用户评论</a></li><li><a href=\""+AdminLink+"manage-categories.php\">分类管理</a></li><li><a href=\""+AdminLink+"manage-tags.php\">标签管理</a></li><li><a href=\""+AdminLink+"manage-medias.php\">文件管理</a></li><li><a href=\""+AdminLink+"manage-users.php\">用户管理</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-gear\"></i>网站设置</a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"options-general.php\">基本设置</a></li><li><a href=\""+AdminLink+"options-discussion.php\">评论设置</a></li><li><a href=\""+AdminLink+"options-reading.php\">阅读设置</a></li><li><a href=\""+AdminLink+"options-permalink.php\">永久链接</a></li></ul><li></ul></div>";
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+AdminLink+"index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-paper-plane\"></i>全局模块<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"profile.php\">个人设置</a></li><li><a href=\""+AdminLink+"plugins.php\">插件控制</a></li><li><a href=\""+AdminLink+"themes.php\">模板外观</a></li><li><a href=\""+AdminLink+"backup.php\">数据备份</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-pencil\"></i>快捷操作<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"write-post.php\">撰写文章</a></li><li><a href=\""+AdminLink+"write-page.php\">创建页面</a></li><li><a href=\""+AdminLink+"user.php\">新增用户</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-cube\"></i>内容管理<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"manage-posts.php\">内容文章</a></li><li><a href=\""+AdminLink+"manage-pages.php\">独立页面</a></li><li><a href=\""+AdminLink+"manage-comments.php\">用户评论</a></li><li><a href=\""+AdminLink+"manage-categories.php\">分类管理</a></li><li><a href=\""+AdminLink+"manage-tags.php\">标签管理</a></li><li><a href=\""+AdminLink+"manage-medias.php\">文件管理</a></li><li><a href=\""+AdminLink+"manage-users.php\">用户管理</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-gear\"></i>网站设置<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"options-general.php\">基本设置</a></li><li><a href=\""+AdminLink+"options-discussion.php\">评论设置</a></li><li><a href=\""+AdminLink+"options-reading.php\">阅读设置</a></li><li><a href=\""+AdminLink+"options-permalink.php\">永久链接</a></li></ul><li></ul></div>";
 	}else if(UserGroup == "editor"||UserGroup == "contributor"){
 		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+AdminLink+"index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+AdminLink+"profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li><li><a href=\""+AdminLink+"write-post.php\"><i class=\"fa fa-pencil\"></i>创建文章</a></li><li><a href=\""+AdminLink+"manage-posts.php\"><i class=\"fa fa-cube\"></i>管理文章</a></li><li><a href=\""+AdminLink+"/manage-comments.php\"><i class=\"fa fa-comments-o\"></i>管理评论</a></li></ul></div>";
     }else{
@@ -83,5 +83,11 @@ $(function(){
     }
 	$("body").on("click",".menu-li",function () {
 		$(this).find(".menu-ul").slideToggle(200);
+	});
+	$("body").on("click","#wmd-fullscreen-button",function () {
+		$(".main").addClass("main-in");
+	});
+	$("body").on("click","#wmd-exit-fullscreen-button",function () {
+		$(".main").removeClass("main-in");
 	});
 });
